2f94e8c53a1b79e0234d279ad0e1c754bceaae42
zhaoyakun <yakun.zhao@derbysoft.com>
2017-06-27 15:36:00 +0800

    springmvc to XML

--- src/main/java/com/ahfdkun/springconfig/SpringMVCJavaConfig.java
+++ src/main/java/com/ahfdkun/springconfig/SpringMVCJavaConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -12,15 +13,16 @@ import com.ahfdkun.controller.IndexController;
 
 @Configuration
 @ComponentScan(basePackageClasses = IndexController.class, includeFilters=@Filter(Controller.class))
+@ImportResource("/WEB-INF/spring-servlet.xml")
 public class SpringMVCJavaConfig {
 
-	@Bean
+	/*@Bean
 	public ViewResolver viewResolver() {
         InternalResourceViewResolver resourceViewResolver = new InternalResourceViewResolver();
         resourceViewResolver.setPrefix("/WEB-INF/view/");
         resourceViewResolver.setSuffix(".jsp");
         resourceViewResolver.setExposeContextBeansAsAttributes(true); // 使得可以在JSP页面中通过${ }访问容器中的bean
         return resourceViewResolver;
-    }
+    }*/
 }
 	
